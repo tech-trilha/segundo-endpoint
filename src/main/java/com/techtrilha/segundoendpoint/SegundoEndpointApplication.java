76de83097dd050198b81df7c1557c3da5a5338b5
Felipe Miranda <felip.simi@gmail.com>
2020-06-13 13:01:00 -0300

    Preparacao: setup do projeto no Spring Initializr

---
+++ src/main/java/com/techtrilha/segundoendpoint/SegundoEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.techtrilha.segundoendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoEndpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techtrilha/segundoendpoint/SegundoEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtrilha.segundoendpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoEndpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
