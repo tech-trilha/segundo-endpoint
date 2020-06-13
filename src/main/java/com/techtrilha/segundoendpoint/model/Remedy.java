341e56408aa79c9976989f2ab0198a4b995ec28b
Felipe Miranda <felip.simi@gmail.com>
2020-06-13 15:40:48 -0300

    Primeiro passo: criacao da classe Remedy

---
+++ src/main/java/com/techtrilha/segundoendpoint/model/Remedy.java
@@ -0,0 +1,23 @@
+package com.techtrilha.segundoendpoint.model;
+
+public class Remedy {
+
+  private Long id;
+  private String name;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
